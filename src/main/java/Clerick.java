f5a81bc5087402621c90f6f59298cab298cd5458
Patryk <patryksmacki@googlemail.com>
2019-08-01 11:24:48 +0100

    clearick and enum for it done

--- src/main/java/Clerick.java
+++ src/main/java/Clerick.java
@@ -1,2 +1,13 @@
-public class Clerick {
+import enums.HealingTool;
+
+public class Clerick extends Person {
+
+    private HealingTool healingTool;
+
+    public Clerick(String name, int health, HealingTool healingTool) {
+        super(name, health);
+        this.healingTool = healingTool;
+    }
+
+
 }
--- src/main/java/MagicPeople.java
+++ src/main/java/MagicPeople.java
@@ -1,2 +1,17 @@
-public abstract class MagicPeople {
+import enums.Creature;
+import enums.Spell;
+
+public abstract class MagicPeople extends Person {
+
+    private Creature creature;
+    private Spell spell;
+
+    public MagicPeople(String name, int health, Creature creature, Spell spell) {
+        super(name, health);
+        this.creature = creature;
+        this.spell = spell;
+    }
+
+
+
 }

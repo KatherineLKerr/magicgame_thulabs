6e50d7ee7c0a8b066990124d4672f045869a974e
Patryk <patryksmacki@googlemail.com>
2019-08-01 11:02:34 +0100

    created warriors and enum

--- src/main/java/Warrior.java
+++ src/main/java/Warrior.java
@@ -1,3 +1,12 @@
-public abstract class Warrior {
+import enums.Weapon;
 
+public abstract class Warrior extends Person{
+
+    private Weapon weapon;
+
+
+    public Warrior(String name, int health, Weapon weapon) {
+        super(name, health);
+        this.weapon = weapon;
+    }
 }
